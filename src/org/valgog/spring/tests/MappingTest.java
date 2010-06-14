6fa7d7631760b38839a4cfc6c57be7506e059723
valgog <valgog@localhost>
2010-06-14 08:48:51 +0200

    Now using JUnit4 for tests

---
+++ src/org/valgog/spring/tests/MappingTest.java
@@ -0,0 +1,108 @@
+package org.valgog.spring.tests;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.valgog.spring.AnnotatedRowMapper;
+import org.valgog.spring.tests.example.SimpleClass;
+
+
+public class MappingTest {
+
+	private Connection conn;
+	
+	@Before
+	public void setUp() throws Exception {
+		// Get connection
+		Class.forName("org.postgresql.Driver");
+		String url = "jdbc:postgresql://localhost/postgres";
+		Properties props = new Properties();
+		props.setProperty("user","postgres");
+		props.setProperty("password","postgres");
+		this.conn = DriverManager.getConnection(url, props);
+		createTestTables(this.conn);
+	}
+
+	private void createTestTables(Connection conn) throws SQLException {
+		Statement s = conn.createStatement();
+		
+		final String SQL = 
+			"DROP SCHEMA IF EXISTS test CASCADE; \n" +
+			"CREATE SCHEMA test; \n" +
+			"CREATE TABLE test.simple ( \n" +
+			"  id integer, \n" +
+			"  name text, \n" +
+			"  country_code text, \n" +
+			"  last_marks int[], \n" +
+			"  tags text[] \n" +
+			"); \n" +
+			"insert into test.simple \n" +
+			"select s.i, 'name' || s.i, 'DE', ARRAY[ (random()*100)::integer,(random()*100)::integer,(random()*100)::integer,(random()*100)::integer ], '{a,b,c,d}'::text[] \n" +
+			"from generate_series(1, 100) as s(i);";
+		
+		s.execute(SQL);
+		conn.commit();
+	}
+	
+	
+	@Test
+	public final void testMapRow() throws SQLException {
+		
+		PreparedStatement ps = conn.prepareStatement("SELECT * FROM test.simple;");
+		ResultSet rs = ps.executeQuery();
+		AnnotatedRowMapper<SimpleClass> mapper = AnnotatedRowMapper.getMapperForClass(SimpleClass.class);
+		int i = 0;
+		while( rs.next() ) {
+			SimpleClass result = mapper.mapRow(rs, i++);
+			assertNotNull(result);
+		}
+	}
+
+	
+	@Test()
+	public void testSingleMapRow() throws SQLException {
+		
+		PreparedStatement ps = conn.prepareStatement("SELECT 1 as id, 'Muster' as name, 'DE' as country_code, '{1,1,3,1,5}'::int4[] as last_marks, '{a,b,c,NULL,e}'::text[] as tags");
+		ResultSet rs = ps.executeQuery();
+		AnnotatedRowMapper<SimpleClass> mapper = AnnotatedRowMapper.getMapperForClass(SimpleClass.class);
+		int i = 0;
+		while( rs.next() ) {
+			SimpleClass result = mapper.mapRow(rs, i++);
+			assertNotNull(result);
+			assertEquals(1, result.getId());
+			assertThat("Muster", is(result.getName()));
+			assertThat("DE", is(result.getCountryCode()));
+			assertThat(new int[] {1, 1, 3, 1, 5}, is(result.getLastMarks()));
+			assertThat(new String[] {"a","b","c",null,"d"}, is(result.getTags()));
+		}
+
+	}	
+	
+	@Test(expected=SQLException.class)
+	public void testPrimitiveMapRow() throws SQLException {
+		
+		PreparedStatement ps = conn.prepareStatement("SELECT 1 as id, 'Muster' as name, 'DE' as country_code, '{1,1,3,1, NULL}'::int4[] as last_marks, '{a,b,c}'::text[] as tags");
+		ResultSet rs = ps.executeQuery();
+		AnnotatedRowMapper<SimpleClass> mapper = AnnotatedRowMapper.getMapperForClass(SimpleClass.class);
+		int i = 0;
+		while( rs.next() ) {
+			mapper.mapRow(rs, i++);
+			// should throw an exception
+		}
+
+	}	
+	
+
+}
